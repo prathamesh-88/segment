8128dd7560753758e61b348a8bd55a1206d43c0e
Prathamesh Tamanekar <prathamesh@WINDELLAP-762.local>
2022-08-08 17:56:16 +0530

    Segment installed

--- js/segmentjs.js
+++ js/segmentjs.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const ctaButton = document.querySelector('#customCarousel1 > div > div.carousel-item.active > div > div > div:nth-child(1) > div > div > a');
-    ctaButton.addEventListener('click', function() {
-        window.analytics.track('ctaClick');
-    });
+    const ctaButtons = document.querySelectorAll('#customCarousel1 .btn1');
+    ctaButtons.forEach((ctaButton) => {
+        ctaButton.addEventListener('click', function() {
+            window.analytics.track('ctaClick');
+        });
+    })
+ 
+
+    const buyButtons = document.querySelectorAll('body > section.shop_section.layout_padding > div > div.row > div > div > a');
+    buyButtons.forEach((buyButton) => {
+        buyButton.addEventListener('click', function() {
+            var price = buyButton.querySelector('h6 > span');
+            price = price.innerHTML.split('$')[1].replace(/\s/g,'');
+            window.analytics.track('buyButtonClick', {
+                amount : price,
+                currency: 'USD',
+                amt: parseFloat(price)
+            });
+        });
+    })
 });
+
+// `body > section.shop_section.layout_padding > div > div.row > div > div > a`
+// `body > section.shop_section.layout_padding > div > div.row > div > div > a`
+// `body > section.shop_section.layout_padding > div > div.row > div > div > a`
