32164e1f43869222556f695fd73c84894a747fef
prathameshwingify <prathamesh.tamanekar@wingify.com>
2022-10-18 14:05:37 +0530

    Fresh Setup

--- js/segmentjs.js
+++ js/segmentjs.js
@@ -1,8 +1,20 @@
-const ctaButton = document.querySelector('#navbarSupportedContent > ul > li:nth-child(3) > a');
+const ctaButton = document.querySelector('#navbarSupportedContent > ul > li:nth-child(4)');
 ctaButton.addEventListener('click', function() {
-    window.analytics.track('ctaClick');
+    console.log('contactNowClick');
+    window.analytics.track('contactNowClick');
 });
 
+const contactForm = document.querySelector('#contactForm');
+contactForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const nameField = document.querySelector("#fullName");
+    console.log(nameField.value);
+    let leadName = nameField.value
+    window.analytics.track('contactFormSubmitted', {
+        leadName
+    });
+    contactForm.reset();
+});
 
 const buyButtons = document.querySelectorAll('body > section.shop_section.layout_padding > div > div.row > div > div > a');
 buyButtons.forEach((buyButton) => {
@@ -10,9 +22,7 @@ buyButtons.forEach((buyButton) => {
         var price = buyButton.querySelector('h6 > span');
         price = price.innerHTML.split('$')[1].replace(/\s/g,'');
         window.analytics.track('buyButtonClick', {
-            amount : price,
-            currency: 'USD',
-            amt: parseFloat(price)
+            amount: parseFloat(price)
         });
     });
 })
