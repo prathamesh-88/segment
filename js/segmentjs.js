a55a9a94056c3e58100bc4e1ab4b61bd07ed0cea
prathameshwingify <prathamesh.tamanekar@wingify.com>
2022-10-18 15:54:55 +0530

    Fresh Setup

--- js/segmentjs.js
+++ js/segmentjs.js
@@ -1,28 +1,35 @@
 const ctaButton = document.querySelector('#navbarSupportedContent > ul > li:nth-child(4)');
-ctaButton.addEventListener('click', function() {
-    console.log('contactNowClick');
-    window.analytics.track('contactNowClick');
-});
+if (ctaButton){
+    ctaButton.addEventListener('click', function() {
+        console.log('contactNowClick');
+        window.analytics.track('contactNowClick');
+    });
+}
 
 const contactForm = document.querySelector('#contactForm');
-contactForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const nameField = document.querySelector("#fullName");
-    console.log(nameField.value);
-    let leadName = nameField.value
-    window.analytics.track('contactFormSubmitted', {
-        leadName
+if (contactForm){
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const nameField = document.querySelector("#fullName");
+        console.log(nameField.value);
+        let leadName = nameField.value
+        window.analytics.track('contactFormSubmitted', {
+            leadName
+        });
+        contactForm.reset();
     });
-    contactForm.reset();
-});
+}
 
 const buyButtons = document.querySelectorAll('body > section.shop_section.layout_padding > div > div.row > div > div > a');
-buyButtons.forEach((buyButton) => {
-    buyButton.addEventListener('click', function() {
-        var price = buyButton.querySelector('h6 > span');
-        price = price.innerHTML.split('$')[1].replace(/\s/g,'');
-        window.analytics.track('buyButtonClick', {
-            amount: parseFloat(price)
+if (buyButtons){
+    buyButtons.forEach((buyButton) => {
+        buyButton.addEventListener('click', function() {
+            console.log("Clicked");
+            var price = buyButton.querySelector('h6 > span');
+            price = price.innerHTML.split('$')[1].replace(/\s/g,'');
+            window.analytics.track('buyButtonClick', {
+                amount: parseFloat(price)
+            });
         });
-    });
-})
+    })
+}
